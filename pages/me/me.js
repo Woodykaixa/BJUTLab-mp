d30b34ac538abf733793dfc37f52863b7e2c0a5e
kaixa <690750353@qq.com>
2020-02-25 19:36:15 +0800

    增加公共样式文件skin.wxss，修改个人中心页面

--- pages/me/me.js
+++ pages/me/me.js
@@ -1,38 +1,32 @@
 const {
     $Toast
 } = require('../../iview/base/index');
-const app=getApp();
+import {
+    promisify
+} from "miniprogram-api-promise";
+const app = getApp();
 Page({
     data: {
         fixedId: "18074104",
-		fixedPw:"18074104",
-        index: "homepage",
-        userInfo: {},
-        hasUserInfo: false,
-        canIUse: wx.canIUse('button.open-type.getUserInfo')
+        fixedPw: "18074104",
+        userInfo: {}
     },
 
-	showToast: () => {
-		$Toast({
-			content: "你点击了一条预约记录"
-		});
-	},
-	handleChange({
-		detail
-	}) {
-		this.setData({
-			current: detail.key
-		});
-	},
-	onLoad:()=>{
-		
-	},
-	getUserInfo: function (e) {
-		console.log(e)
-		app.globalData.userInfo = e.detail.userInfo
-		this.setData({
-			userInfo: e.detail.userInfo,
-			hasUserInfo: true
-		})
-	}
+    showToast: () => {
+        $Toast({
+            content: "你点击了一条预约记录"
+        });
+    },
+    onLoad: function() {
+        let self = this; //这里要提前声明变量保存this。下面不能直接写this.setData，因为this指向的对象已经发生了变化。
+        wx.getUserInfo({
+            withCredentials: false,
+            lang: 'zh_CN',
+            success: function(res) {
+                self.setData({
+                    userInfo: res.userInfo
+                });
+            }
+        })
+    }
 })
