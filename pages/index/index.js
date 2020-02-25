7b9c1f983ca7a343925febede06b870b00fa69e6
kaixa <690750353@qq.com>
2020-02-25 21:20:06 +0800

    底部修改底部导航栏样式，绘制通知界面，电脑没电了预约界面只做了一点

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,16 +2,6 @@ const app = getApp();
 
 Page({
     data: {
-        fixedId: "18074104",
-        index: "homepage",
-        userInfo: {},
-        hasUserInfo: false,
-        canIUse: wx.canIUse('button.open-type.getUserInfo')
-    },
-    //事件处理函数
-    bindViewTap: function() {
-        wx.navigateTo({
-            url: '../logs/logs'
-        })
+
     }
 });
